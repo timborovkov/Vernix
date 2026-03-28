78da81a09b563dff39570a1211a886ade72b30dd
Tim Borovkov <tim.borovkov@icloud.com>
2026-03-29 00:24:49 +0700

    Update page.tsx

--- src/app/(public)/features/context/page.tsx
+++ src/app/(public)/features/context/page.tsx
@@ -48,7 +48,7 @@ const SUPPORTED_FORMATS = [
 
 export default function ContextPage() {
   return (
-    <div className="mx-auto max-w-5xl px-4 py-28 lg:py-32">
+    <div className="mx-auto max-w-5xl px-4 pt-10 pb-28 lg:pt-12 lg:pb-32">
       {/* Hero */}
       <div className="relative mb-24 px-6 py-10 text-center lg:mb-28">
         <HeroBg />
